f493efd94c027e9b4cebd0e02faa7a390371f78e
Baumbart13 <tkefer@tsn.at>
2022-11-21 21:22:53 +0100

    Implemented Signatures finally

--- SevCashIF.Core/Models/Signatures/Austria.cs
+++ SevCashIF.Core/Models/Signatures/Austria.cs
@@ -1,3 +1,23 @@
 namespace SevCashIF.Core.Models.Signatures;
 
-public class Austria : ISignature { }
+public class Austria : ISignature {
+    /// <summary>
+    /// Signature (QR) Code
+    /// </summary>
+    public string SignatureCode { get; set; } = string.Empty;
+    
+    /// <summary>
+    /// Signature text
+    /// </summary>
+    public string SignatureText { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Cancellation signature (QR) code
+    /// </summary>
+    public string SignatureCancellationCode { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Cancellation signature text
+    /// </summary>
+    public string SignatureCancellationText { get; set; } = string.Empty;
+}
--- SevCashIF.Core/Models/Signatures/Germany.cs
+++ SevCashIF.Core/Models/Signatures/Germany.cs
@@ -1,3 +1,23 @@
 namespace SevCashIF.Core.Models.Signatures;
 
-public class Germany : ISignature { }
+public class Germany : ISignature {
+    /// <summary>
+    /// QR code string
+    /// </summary>
+    public string TseQrCode { get; set; } = string.Empty;
+
+    /// <summary>
+    /// TSE transaction data
+    /// </summary>
+    public List<Int16> TseData { get; set; }
+
+    /// <summary>
+    /// QR code string of cancellation
+    /// </summary>
+    public string TseCancellationQrCode { get; set; } = string.Empty;
+
+    /// <summary>
+    /// TSE transaction data cancellation
+    /// </summary>
+    public List<Int16> TseCancellationData { get; set; }
+}
