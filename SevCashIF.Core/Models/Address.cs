d16030fef1c646126015e51a7baa08854a7d5ea0
Baumbart13 <tkefer@tsn.at>
2022-11-21 21:16:09 +0100

    Fixed addresses

--- SevCashIF.Core/Models/Address.cs
+++ SevCashIF.Core/Models/Address.cs
@@ -1,5 +1,8 @@
 namespace SevCashIF.Core.Models; 
 
 public class Address {
-    
+    public string Street { get; set; } = string.Empty;
+    public string HouseNumber { get; set; } = string.Empty;
+    public string PostalCode { get; set; } = string.Empty;
+    public string CityName { get; set; } = string.Empty;
 }
