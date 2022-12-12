e2d28fce16e8f0b1a0e73fad60dd455a5f95c43c
Baumbart13 <tkefer@tsn.at>
2022-12-12 10:12:57 +0100

    Added Cancellation Details as an object

---
+++ SevCashIF.Core/Models/CancellationDetails.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace SevCashIF.Core.Models;
+
+public class CancellationDetails {
+    public BigInteger Number { get; set; }
+    public string Cashier { get; set; } = string.Empty;
+    public BigInteger CashierId { get; set; }
+    public DateTime Timestamp { get; set; }
+    public string Text { get; set; } = string.Empty;
+}
